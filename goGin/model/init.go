432ab95903dbbd71c97dc0cd4971fb169e08f6c8
MitsuhaOma <2391542095@qq.com>
2019-12-16 10:09:25 +0800

    new file

--- goGin/model/init.go
+++ goGin/model/init.go
@@ -1,20 +1,45 @@
 package model
 
 import (
-       "fmt"
+       "log"
        "github.com/jinzhu/gorm"
        _"github.com/jinzhu/gorm/dialects/mysql"
        _"github.com/go-sql-driver/mysql"
 )
 
-var Db, err1 = gorm.Open("mysql", "username:password@/dbname?charset=utf8&parseTime?&loc=Local")
+const dns = "root:password@/goweb?charset=utf8&parseTime=True&loc=Local"
 
-func Init() {
-     if err1 != nil {
-	fmt.Println(err)
-	panic("连接数据库失败")
-     }
-     defer Db.Close()
+type Database struct {
+     Self *gorm.DB
 }
+
+var Db *Database
+
+func getDatabase() (*gorm.DB, error) {
+     db, err := gorm.Open("mysql", dns)
+     if err != nil {
+	log.Fatal(err)
+	return nil, err
+     }
+     return db, nil
+}
+
+func (db *Database) Init() {
+     newDb, err := getDatabase()
+     if err != nil {
+	log.Print("数据库初始化错误")
+	log.Println(err)
+     }
+     Db = &Database{Self:newDb}
+}
+
+func (db *Database) Close() {
+     if err := Db.Self.Close(); err != nil {
+	log.Print("数据库关闭错误")
+	log.Println(err)
+     }
+     return
+}
+
      
      
