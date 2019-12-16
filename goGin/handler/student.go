d408bb70b4e67a4c3ade172c63477358e3db014b
MitsuhaOma <2391542095@qq.com>
2019-12-16 18:56:56 +0800

    new file

--- goGin/handler/student.go
+++ goGin/handler/student.go
@@ -5,8 +5,11 @@ import(
       "github.com/MitsuhaOma/goproject/goGin/model"
 )
 
+type students struct {
+     Student []string `json:"student"`
+}
 
-func CreatStudent(c *gin.Context) {
+func CreateStudent(c *gin.Context) {
      var tmpStudent students
      if err := c.BindJSON(&tmpStudent); err != nil {
 	c.JSON(400,gin.H{
@@ -14,12 +17,65 @@ func CreatStudent(c *gin.Context) {
         })
 	return
      }
-										for _,v := range tmpStudent.Student {
+     for _,v := range tmpStudent.Student {
 	 c.JSON(200,gin.H{
-		"message":model.CreatStudent(v),
+		"message":model.CreateStudent(v),
 	 })
      }
      c.JSON(200,gin.H{
 	    "message":"Creat Success",
      })
 }
+
+func NewCourseToStudent(c *gin.Context) {
+     var tmpstudent Student
+     if err := c.BindJSON(&tmpstudent);err != nil {
+	c.JSON(400, gin.H{
+	      "message":"Bad Request!",
+	})
+	return
+     }
+     NewCourseToStudent(tmpstudent.Course, tmpstudent.StudentName)
+     c.JSON(200, gin.H{
+	   "message":"Success!",
+     })
+}
+
+func StudentCourse(c *gin.Context) {
+     var course students
+     if err := c.BindJSON(&course); err != nil {
+	c.JSON(400, gin.H{
+	      "message": "Bad Request!",
+	})
+	return
+     }
+     for _, value := range course.Student {
+	 c.JSON(200, gin.H{
+		"message": model.StudentCourse(value),
+         })
+     }
+     c.JSON(200, gin.H{
+           "message":"Query Success",
+     })
+}
+
+func StudentClassroom(c *gin.Context) {
+     var classroom students
+     if err := c.BindJSON(&classroom); err != nil {
+	c.JSON(400, gin.H{
+	      "message": "Bad Request!",
+	})
+	return
+     }
+     for _, value := range classroom.Student {
+	 c.JSON(200, gin.H{
+		"message": model.StudentClassroom(value),
+         })
+     }
+     c.JSON(200, gin.H{
+           "message":"Query Success",
+     })
+}
+
+	 
+
