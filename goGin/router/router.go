88753801a497af85c929439d6b562f3c10f07273
MitsuhaOma <2391542095@qq.com>
2019-12-16 19:15:59 +0800

    new file

--- goGin/router/router.go
+++ goGin/router/router.go
@@ -9,8 +9,13 @@ var Router *gin.Engine
 
 func InitRouter() {
      Router = gin.Default()
-     Router.POST("/student/create",handler.CreateStudent)
-  
+     Router.POST("/student/create", handler.CreateStudent)
+     Router.POST("/classroom/create", handler.CreateClassroom)
+     Router.POST("/course/create", handler.CreateCourse)
+     Router.POST("/student/assignment", handler.NewCourseToStudent)
+     Router.POST("/student/allcourses", handler.StudentCourse)
+     Router.POST("/student/allclassrooms", handler.StudentClassroom)
+
 
      return
 }
